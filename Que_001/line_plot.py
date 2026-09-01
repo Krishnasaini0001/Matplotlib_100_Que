c1e25818a85d3c4b21fd7295d2621dd04b197c1f
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-01 16:50:49 +0530

    Line plot

---
+++ Que_001/line_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+hit_movies = ["RRR", "chhaava", "Dhurandar", "KGF", "12th Fail", "Super 30", "MS Dhoni"]
+revenue = [350, 270, 690, 280, 312, 456, 400]
+
+plt.plot(hit_movies, revenue)
+plt.xlabel("Hit Movies")
+plt.ylabel("Revenue (in $M)")
+plt.title("Movie Revenue Over the Years")
+plt.show()
