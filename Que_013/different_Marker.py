d68a297e2c1ce9b28b53fa1462001c344531d35f
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-02 13:13:57 +0530

    Scatter Plot with Different Marker

---
+++ Que_013/different_Marker.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+Years = [2016, 2017, 2018, 2019, 2020, 2021, 2022]
+GDP_growth = [8.26, 6.8, 6.45, 3.87, -6.78, 9.69, 7.61]
+
+
+plt.scatter(Years, GDP_growth, marker="^")
+
+plt.xlabel("Years")
+plt.ylabel("GDP Growth (in %)")
+plt.title("Bharat GDP Growth Over Years")
+plt.show()
