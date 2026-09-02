301ba5cbb85a3ed4764becff652ce8d6784fd8c8
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-02 13:27:26 +0530

    Histogram with more bins

---
+++ Que_015/With_more_bins.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+marks = [45, 50, 55, 60, 60, 65, 70, 70, 75, 80, 85, 90, 95, 100, 99, 100, 85, 60, 95, 98, 70, 52, 54, 89]
+
+plt.hist(marks, bins=15)
+
+plt.xlabel("Marks")
+plt.ylabel("Number of Students")
+plt.title("Marks Distribution")
+plt.show()
